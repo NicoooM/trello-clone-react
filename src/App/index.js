c1ed62ee09b766783ede0769abc348ae2f7bdea3
NicoooM <nmarsan33000@gmail.com>
2021-07-25 19:04:29 +0200

    scroll with mouse

--- src/App/index.js
+++ src/App/index.js
@@ -2,9 +2,10 @@ import Layout from "../components/Layout";
 import Header from "../components/Header";
 import List from "../components/List";
 import styles from "./App.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  let list_container = styles.list_container;
   const [lists, setLists] = useState([]);
   const [listKey, setListKey] = useState(1);
   const deleteList = (id) => {
@@ -17,15 +18,42 @@ export default function App() {
       <List key={listKey} id={listKey} handleDelete={deleteList} />,
     ]);
   };
+  useEffect(() => {
+    const container = document.querySelector("." + list_container);
+    let mouseDown = false;
+    let pos = { left: 0, x: 0 };
+    const mouseDownHandler = (e) => {
+      mouseDown = true;
+      console.log("down");
+      pos = {
+        left: container.scrollLeft,
+        x: e.clientX,
+      };
+      document.addEventListener("mousemove", mouseMoveHandler);
+    };
+    const mouseMoveHandler = (e) => {
+      if (mouseDown === true) {
+        const dx = e.clientX - pos.x;
+        container.scrollLeft = pos.left - dx;
+      }
+      if (mouseDown === false) {
+        container.removeEventListener("mousemove", mouseMoveHandler);
+      }
+    };
+    if (container !== null) {
+      container.addEventListener("mousedown", mouseDownHandler);
+      container.addEventListener("mouseup", () => (mouseDown = false));
+    }
+  });
   return (
     <Layout>
       <Header createList={createList} />
       {lists.length === 0 ? (
         <p className={styles.indication}>
-          To add a list, click on the Add button
+          To add a list, click on the Add a list button
         </p>
       ) : (
-        <ul className={styles.list_container}>{lists}</ul>
+        <ul className={list_container}>{lists}</ul>
       )}
     </Layout>
   );
